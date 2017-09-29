d7a5cf112ef1bf0cc2ef6bd9a27bc17abd7ba658
Nick <nchin@villanova.edu>
2017-09-29 17:35:27 -0400

    Updated anime list document

---
+++ Anime Suggestions/src/AnimeSuggestion.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class AnimeSuggestion {
+
+	//Text document name
+	public static final String anime = ".\\src\\AnimeText";
+	public static final int DATA = 4; //The number of anime in the database
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		int count = 0;
+		Scanner file = new Scanner (new File(anime));
+		String line;
+		String[] info;
+		String[] category;
+		Anime[] database = new Anime[DATA];
+		
+		while (file.hasNext()){
+			line = file.nextLine();
+			
+			info = line.split("-");
+			//System.out.println("-"+info[0]+"-");			//Prints anime name
+			
+			category = info[1].split(",");
+			for(int i=0; i<category.length; i++){			//Prints anime categories
+				//System.out.println(category[i]);
+			}
+			
+			Anime temp = new Anime(info[0], category);
+			database[count] = temp;
+			count++;
+		}
+		//Test compareGen method
+		System.out.println("\n" + database[2].compareGen(database[2], database[3]));
+		
+		//Test randomizer
+		Anime[] test = randomizer(database);
+		for (int i=0; i<test.length; i++){
+			System.out.println (test[i].getName());
+		}
+		
+	}
+
+	public static Anime[] randomizer(Anime[] a)
+	   {
+	      int num = 0;
+	      Anime[] temp = new Anime[DATA];
+	      Random rand = new Random();
+	      int random = 0;
+	      int[] generatedNums = new int[DATA];
+	      boolean repeat = false;
+	      
+	      for (int i=0; i<DATA; i++)   //initialize array to all -1
+	         generatedNums[i] = -1;
+	      
+	      while (num < DATA)
+	      {
+	         repeat = false;
+	         random = rand.nextInt(DATA);  // generate random int
+	         for (int i=0; i<DATA; i++)  //check for duplicates
+	            if (generatedNums[i] == random)
+	               repeat = true;
+	         
+	         if (repeat == false) //if no repeat, update arrays
+	         {
+	            generatedNums[num] = random;
+	            temp[num] = a[random];
+	            num++;
+	         }
+	      }
+	      return temp;
+	   }
+
+}
